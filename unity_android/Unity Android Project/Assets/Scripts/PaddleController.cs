3a3e73a119b36538620caf4097b0cd014987cace
vonnenaut <danielashcom@gmail.com>
2021-01-10 21:27:57 +0700

    automated sync commit for unity_learning

---
+++ unity_android/Unity Android Project/Assets/Scripts/PaddleController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float moveSpeed;
+
+    private void Awake() 
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TouchMove()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            
+        }
+    }
+}
